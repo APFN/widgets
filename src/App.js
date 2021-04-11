e265af96cf3d322dacc9ea888d2c869f3d30b8c6
APFN <alvarodenegreiros@gmail.com>
2021-04-11 19:33:06 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import Accordion from  './components/Accordion.js';
+
+const items  = [
+    {
+        title: 'What is react?2',
+        content: 'Blanedendedned e duende db ewdebd '
+    },
+    {
+        title: 'What is react?3',
+        content: 'Blanedendedned e duende db ewdebd '
+    },
+    {
+        title: 'What is react?4',
+        content: 'Blanedendedned e duende db ewdebd '
+    }
+];
 
 export default () => {
-    return <h1> Widgets App</h1>;
+    return <div>
+        <Accordion items={items}/>
+    </div>;
 };
 
